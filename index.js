cc578eff7abe136ff1dd85e828ce67d53700fa5a
SSK <kasinaat.ss@zohocorp.com>
2021-01-16 16:55:18 +0530

    port variable added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 
 var app = express();
-
-app.listen(3000,function(){
+var port = process.env.PORT || 3000;
+app.listen(port,function(){
     console.log("App started in port 3000");
 });
 
